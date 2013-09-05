b6d52e03d5faacf71f92e0da9923556c8ff134e9
h5lium <h5lium@163.com>
2013-09-05 15:37:11 +0800

    numLoaded

---
+++ test/client.test.js
@@ -0,0 +1,75 @@
+
+var Client = require('../../js/Client.class');
+var domino = require('domino'),
+    Zepto = require('zepto-node'),
+    $ = Zepto(domino.createWindow());
+
+
+var client = new Client('jwc.wyu.cn', 'gbk');
+var loginForm = {
+    'UserCode': '11080731',
+    'UserPwd': 'lliang',
+    'Validate': ''
+}
+
+// 进入登录页面
+client.get('/student/', {}, {}, function(err, res, body){
+    // 提交登录信息
+    client.cookie['LogonNumber'] = '';
+    client.post('/student/logon.asp', loginForm, {}, function(err, res, body){
+        var success = /welcome/.test(body);
+        console.log(success ? '登录成功' : '登录失败');
+
+        client.get('/student/f1.asp', {}, {}, function(err, res, body) {
+            var profile = parseProfile(body);
+            console.dir(profile);
+        }).get('/student/f3.asp', {}, {}, function(err, res, body){
+            var courses = parseCourses(body);
+            console.dir(courses);
+        });
+    });
+});
+
+
+function parseProfile(html) {
+    html = html.replace(/\r\n/g, '');
+    var tableHtml = html.match(/<table.+<\/table>/)[0],
+        $table = $(tableHtml),
+        $trs = $table.find('tr');
+
+    return {
+        code: $trs.eq(0).find('td').eq(1).text(),
+        name: $trs.eq(0).find('td').eq(3).text(),
+        avatar: $trs.eq(0).find('td').eq(6).find('img').attr('src'),
+        politics: $trs.eq(2).find('td').eq(3).text(),
+        sex: $trs.eq(1).find('td').eq(1).text(),
+        birth: $trs.eq(1).find('td').eq(3).text(),
+        nation: $trs.eq(1).find('td').eq(5).text(),
+        major: $trs.eq(3).find('td').eq(1).text(),
+        class: $trs.eq(3).find('td').eq(3).text(),
+        college: $trs.eq(4).find('td').eq(1).text(),
+        entrance: $trs.eq(5).find('td').eq(3).text(),
+        dormitory: $trs.eq(8).find('td').eq(5).text()
+    }
+}
+function parseCourses(html) {
+    html = html.replace(/\r\n/g, '')
+        .replace(/&nbsp;/g, ' ');
+    var tableHtml = html.match(/<TABLE cellSpacing=0 borderColorDark=#eeeeee cellPadding=2 width="99%".+<\/TABLE>/)[0],
+        $table = $(tableHtml),
+        $trs = $table.find('tr');
+
+    var courses = [];
+    var periods = 5,
+        days = $trs.eq(0).find('td').length - 1;
+    for (var i=0, j, $tds; i<periods; i++) {
+        courses[i] = [];
+        $tds = $trs.eq(i + 1).find('td');
+        for (j=0; j<days; j++) {
+            courses[i][j] = $tds.eq(j + 1).html()
+                .replace(/<br>/g, '\n')
+                .replace(/&nbsp;/g, ' ');
+        }
+    }
+    return courses;
+}
